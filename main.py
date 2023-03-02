22f3aae8cd42123f412a72f5ac85281f035cb33a
malukawasaki <115004497+malukawasaki@users.noreply.github.com>
2023-03-02 12:13:51 +0100

    added title, name,student number header. Add your info on it please

---
+++ main.py
@@ -0,0 +1,7 @@
+#GEO5017 Assignment 1: Linear Regression
+#Dimitris Mantas 5836670
+#Maria Luisa Tarozzo Kawasaki 5620341
+#Dennis (last name) (student number)
+
+#TO DO: Add Dimitris initial code
+
