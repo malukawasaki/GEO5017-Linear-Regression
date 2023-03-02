6072be1d4d4c79d9b856611f6a1ca86999f738d4
malukawasaki <115004497+malukawasaki@users.noreply.github.com>
2023-03-02 12:16:47 +0100

    header completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #GEO5017 Assignment 1: Linear Regression
 #Dimitris Mantas 5836670
 #Maria Luisa Tarozzo Kawasaki 5620341
-#Dennis (last name) (student number)
+#Dennis Lagendijk 4587693
 
 #TO DO: Add Dimitris initial code
 
