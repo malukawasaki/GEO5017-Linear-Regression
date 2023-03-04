51648e4966f29276b4b2d7b8a06cc2f9d105d309
Dimitris Mantas <dimitris.mantas@outlook.com>
2023-03-04 18:09:07 +0100

    Fix Bug in Solution Notebook

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+@
+requirements.txt,1\9\19359a61ae2446b51b549167b014da2fcf265768
--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 
+# TODO - Make sure that the gradient and residual error are EXACTLY THE SAME as in the notebook.
 # Observations
 P = np.array([[+2.00, +0.00, +1.00],
               [+1.08, +1.68, +2.38],
@@ -38,6 +39,7 @@ def gradient_descent(objective_arguments, objective_gradient, learning_rate=1e-5
     return objective_arguments
 
 model_weights_cons_speed = gradient_descent(W_cons_speed, gradient)
+# TODO - Consider defining a function which computes the residual error regardless of the dimension of the problem.
 res_error_cons_speed = np.sum((P - T @ W_cons_speed) ** 2)
 
 print("Model Weights for costant speed: \n", model_weights_cons_speed)
@@ -49,9 +51,13 @@ W_cons_acc = np.ones((3, 3))
 # Time
 T2 = np.array([[x ** n for n in range(3)] for x in [1, 2, 3, 4, 5, 6]]).reshape(len(P), -1)
 
+
+# TODO - Consider deleting this function.
 def gradient_cons_acc(objective_arguments):
     return 2 * T2.T @ (T2 @ objective_arguments - P)
 
+
+# TODO - Consider replacing gradient_cons_acc with gradient.
 model_weights_cons_acc = gradient_descent(W_cons_acc, gradient_cons_acc)
 res_error_cons_acc = np.sum((P - T2 @ W_cons_acc) ** 2)
 
