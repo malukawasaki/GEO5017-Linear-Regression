c54ac9c97d96991f467ee9e7c2d32f3a9261d8f7
malukawasaki <115004497+malukawasaki@users.noreply.github.com>
2023-03-06 22:27:58 +0100

    Model weights use the same gradient desc function and res errors also use the same res error function

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 
 import numpy as np
 
-# TODO - Make sure that the gradient and residual error are EXACTLY THE SAME as in the notebook.
 # Observations
 P = np.array([[+2.00, +0.00, +1.00],
               [+1.08, +1.68, +2.38],
@@ -14,54 +13,49 @@ P = np.array([[+2.00, +0.00, +1.00],
               [-1.31, -1.51, +2.59],
               [+0.57, -1.91, +4.32]], dtype=float)
 
-#Question 1.Plot the trajectory through these data points with your tool of choice.
+# Question 1. Plot the trajectory through these data points with your tool of choice.
 #TODO Insert the code we used for plotting
 
-#Question 2. Linear Regression Exercises
+# Question 2. Linear Regression Exercises
 
-#Costant speed
+# Time constant speed
+T1 = np.array([[x ** n for n in range(2)] for x in [1, 2, 3, 4, 5, 6]]).reshape(len(P), -1)
 
-T = np.array([[x ** n for n in range(2)] for x in [1, 2, 3, 4, 5, 6]]).reshape(len(P), -1)
+# Time constant acc
+T2 = np.array([[x ** n for n in range(3)] for x in [1, 2, 3, 4, 5, 6]]).reshape(len(P), -1)
 
 W_cons_speed = np.ones((2, 3))
 
-def gradient(objective_arguments):
-    # dE/dW = 2X(XW-T) using the chain rule, but the dimensions of X and (XW-T) are not compatible, so X is transposed.
-    return 2 * T.T @ (T @ objective_arguments - P)
+W_cons_acc = np.ones((3, 3))
 
-def gradient_descent(objective_arguments, objective_gradient, learning_rate=1e-5, max_num_iterations=1e+10,
+def gradient(objective_arguments, time):
+    # dE/dW = 2X(XW-T) using the chain rule, but the dimensions of X and (XW-T) are not compatible, so X is transposed.
+    return 2 * time.T @ (time @ objective_arguments - P)
+
+def gradient_descent(objective_arguments, objective_gradient, time, learning_rate=1e-5, max_num_iterations=1e+10,
                      step_eps=1e-15):
     for i in range(int(max_num_iterations)):
-        step = learning_rate * objective_gradient(objective_arguments)
+        step = learning_rate * objective_gradient(objective_arguments, time)
         if np.amax(np.abs(step)) < step_eps:
             break
         objective_arguments -= step
     return objective_arguments
 
-model_weights_cons_speed = gradient_descent(W_cons_speed, gradient)
-# TODO - Consider defining a function which computes the residual error regardless of the dimension of the problem.
-res_error_cons_speed = np.sum((P - T @ W_cons_speed) ** 2)
+model_weights_cons_speed = gradient_descent(W_cons_speed, gradient, T1)
+
+model_weights_cons_acc = gradient_descent(W_cons_acc, gradient, T2)
+
+# Compute residual error
+def res_error(time,weight):
+    return np.sum((P - time @ weight) ** 2)
+
+res_error_cons_speed = res_error(T1,W_cons_speed)
+
+# Lower error compared to constant speed
+res_error_cons_acc = res_error(T2,W_cons_acc)
 
 print("Model Weights for costant speed: \n", model_weights_cons_speed)
 print("Residual Error for costant speed: \n", res_error_cons_speed)
 
-#Costant acceleration
-W_cons_acc = np.ones((3, 3))
-
-# Time
-T2 = np.array([[x ** n for n in range(3)] for x in [1, 2, 3, 4, 5, 6]]).reshape(len(P), -1)
-
-
-# TODO - Consider deleting this function.
-def gradient_cons_acc(objective_arguments):
-    return 2 * T2.T @ (T2 @ objective_arguments - P)
-
-
-# TODO - Consider replacing gradient_cons_acc with gradient.
-model_weights_cons_acc = gradient_descent(W_cons_acc, gradient_cons_acc)
-res_error_cons_acc = np.sum((P - T2 @ W_cons_acc) ** 2)
-
 print("Model Weights for costant acceleration: \n", model_weights_cons_acc)
 print("Residual Error for costant acceleration: \n", res_error_cons_acc)
-
-
